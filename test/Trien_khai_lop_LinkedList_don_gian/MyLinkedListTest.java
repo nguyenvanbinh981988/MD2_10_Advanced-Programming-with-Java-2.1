c3bf849162a0002ec4b7f79b374b4441878a27f6
nguyenvanbinh981988 <binhnguyenvan981988@gmail.com>
2022-05-20 14:19:49 +0700

    new

---
+++ test/Trien_khai_lop_LinkedList_don_gian/MyLinkedListTest.java
@@ -0,0 +1,19 @@
+package Trien_khai_lop_LinkedList_don_gian;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedListTest {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(10);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(3,9);
+        ll.add(14,8);
+        ll.printList();
+
+    }
+
+}
